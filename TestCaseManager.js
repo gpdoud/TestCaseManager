1b75ea04d5d55e04925dce4499e8a38b8f646b9e
Greg doud <gpdoud@gmail.com>
2017-08-26 22:21:06 -0400

    converted from typescript to javascript

--- TestCaseManager.js
+++ TestCaseManager.js
@@ -1,26 +1,22 @@
-"use strict";
-exports.__esModule = true;
-var TestCaseManager = (function () {
-    function TestCaseManager() {
+
+function testResult(expected, actual, desc = null) {
+    return {
+        expected: expected,
+        actual: actual,
+        desc: desc
     }
-    TestCaseManager.prototype.testResult = function (expected, actual, desc) {
-        if (desc === void 0) { desc = null; }
-        return {
-            expected: expected,
-            actual: actual,
-            desc: desc
-        };
-    };
-    TestCaseManager.prototype.runTests = function (tests) {
-        var testSuite = true;
-        for (var _i = 0, tests_1 = tests; _i < tests_1.length; _i++) {
-            var test = tests_1[_i];
-            var rc = test();
-            console.log("Test:", test.prototype.name, "expected[", rc.expected, "],actual[", rc.actual, "],result[", rc.expected == rc.actual ? "passed ]" : "failed ]");
-            testSuite = testSuite && (rc.expected == rc.actual);
-        }
-        return testSuite;
-    };
-    return TestCaseManager;
-}());
-exports.TestCaseManager = TestCaseManager;
+}
+
+function runTests(tests) {
+
+    var testSuiteResult = true;
+    for(test of tests) {
+        var rc = test();
+        console.log("Test:", test.name, "exp[", rc.expected, "],act[", rc.actual, 
+            "],result[", rc.expected == rc.actual ? "PASS ]" : "FAIL ]");
+        testSuiteResult = testSuiteResult && (rc.expected == rc.actual);
+    }
+    return testSuiteResult;
+
+}
+
